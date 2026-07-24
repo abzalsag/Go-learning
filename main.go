70ab1947b6056b17d4b1b3b2399325430e940926
abzal <abzalsag@gmail.com>
2026-07-24 20:00:53 +0500

    lll

--- main.go
+++ main.go
@@ -1,16 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
-func main() {
-	var num int
-	fmt.Scan(&num)
-	if num < 0 {
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
 
-		fmt.Println("num is negative")
-	} else if num == 0 {
-		fmt.Println("num is zero")
-	} else if num > 0 {
-		fmt.Println("num is positive")
+var users []User
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Error creating user", http.StatusBadRequest)
+		return
 	}
+	users = append(users, user)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	if len(users) == 0 {
+		http.Error(w, "No users found", http.StatusNotFound)
+		return
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+func updateUser(w http.ResponseWriter, r *http.Request) {
+	if len(users) == 0 {
+		http.Error(w, "No users updated", http.StatusNotFound)
+		return
+	}
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, "Error updaiting user", http.StatusBadRequest)
+		return
+
+	}
+	users[0] = user
+	json.NewEncoder(w).Encode(user)
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	if len(users) == 0 {
+		http.Error(w, "No users deleted", http.StatusNotFound)
+		return
+	}
+	users = users[1:]
+	fmt.Fprintln(w, "Deleting user")
+
+}
+
+func userHander(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getUsers(w, r)
+	case http.MethodPost:
+		createUser(w, r)
+	case http.MethodPut:
+		updateUser(w, r)
+	case http.MethodDelete:
+		deleteUser(w, r)
+	default:
+		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+
+	}
+
+}
+func main() {
+	http.HandleFunc("/users", userHander)
+	fmt.Println("starrt server")
+	http.ListenAndServe(":8080", nil)
+
 }
